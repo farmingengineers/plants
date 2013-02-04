c09d4cd690c561cf501b0a17f42a43bb4644c188
Matt Burke <spraints@gmail.com>
2013-02-04 00:25:38 -0500

    Just one curl::easy instance

--- app/models/catalog/fedco.rb
+++ app/models/catalog/fedco.rb
@@ -7,16 +7,15 @@ class Catalog
       cookiejar = Rails.root.join('tmp/fedco_cookies').to_s
       FileUtils.safe_unlink(cookiejar)
       with_catalog("Fedco Seeds", "http://www.fedcoseeds.com/") do |catalog|
+        curl = Curl::Easy.new
+        curl.follow_location = true
+        curl.enable_cookies = true
+        curl.cookiejar = cookiejar
+        curl.on_success { |easy| puts "Saving #{easy.url}" ; catalog.catalog_pages.create!(:url => easy.url, :body => easy.body_str) }
+        curl.on_failure { |easy| "ERROR: #{easy.status} #{easy.url}" }
         1.upto(6499) do |item_id|
-          url = "#{catalog.url}seeds/search.php?item=#{item_id}"
-          c = Curl::Easy.new(url) do |curl|
-            curl.follow_location = true
-            curl.enable_cookies = true
-            curl.cookiejar = cookiejar
-            curl.on_success { |easy| puts "Saving #{url}" ; catalog.catalog_pages.create!(:url => url, :body => easy.body_str) }
-            curl.on_failure { |easy| "ERROR: #{easy.status} #{url}" }
-          end
-          c.perform
+          curl.url = "#{catalog.url}seeds/search.php?item=#{item_id}"
+          curl.perform
         end
       end
     end
