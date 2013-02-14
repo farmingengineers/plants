43d55fd6b532129496bec79b753e09fa2f919773
Matt Burke <spraints@gmail.com>
2013-02-14 10:10:30 -0500

    Parse fedco.

--- app/models/catalog/fedco.rb
+++ app/models/catalog/fedco.rb
@@ -23,6 +23,34 @@ class Catalog
     end
 
     def parse_page(catalog_page, doc)
+      name = doc.css('title').first.text.sub(/^Fedco Seeds - /, '')
+      if name != 'Catalog Search'
+        order_form = doc.css('form[name=order]').first
+        description_cell = order_form.css('td')[1]
+        name = description_cell.css('strong').first.text.gsub(/\s+/, ' ')
+        description = description_cell.css('.times14').text
+
+        item = catalog_page.catalog_items.new
+        item.name = name
+        item.description = description
+        item.organic = name =~ /OG/
+        item.save
+
+        collect_prices(item, order_form)
+      end
+    end
+
+    def collect_prices(item, order_form)
+      desc_table = order_form.css('table')[0]
+      desc_table.css('font').each do |price|
+        if price.text.strip =~ /^([A-Z])=(.*) for \$([\d.]+)/
+          name, quantity, price = $1, $2, $3
+          catalog_price = CatalogPrice.new
+          catalog_price.quantity = "#{name} #{quantity}"
+          catalog_price.cents = price.to_f * 100
+          item.catalog_prices << catalog_price
+        end
+      end
     end
   end
 end
